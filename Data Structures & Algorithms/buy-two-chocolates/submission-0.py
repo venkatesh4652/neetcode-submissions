8f935a427b72cd01041cf52999d6aa7e802b177e
venkatesh4652 <tsatyavenkatesh@gmail.com>
2026-07-27 18:37:35 -0400

    Add: buy-two-chocolates - submission-0

---
+++ Data Structures & Algorithms/buy-two-chocolates/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        min1 = min2 = float('inf')
+        for p in prices:
+            if p < min1:
+                min1,min2 = p,min1
+            elif p < min2:
+                min2 = p
+            leftover = money - min1 - min2
+        return leftover if leftover >= 0 else money
+
